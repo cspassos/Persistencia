dabdbdaeb68823ba54ffd6df625e516ef0e8e60e
cspassos <cspassos@latam.stefanini.com>
2018-06-29 16:49:36 -0300

    50% - classeCompra

---
+++ modelo_persistencia/src/main/java/com/persistenciaJPA/model/CompraID.java
@@ -0,0 +1,68 @@
+package com.persistenciaJPA.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class CompraID implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "numero_nota", nullable = false)
+	private Integer numeroNota;
+	
+	@ManyToOne
+	@JoinColumn(name = "pessoaJuridica", referencedColumnName = "id")
+	private PessoaJuridica pessoaJuridica;
+	
+	public CompraID() {
+	}
+	
+	public Integer getNumeroNota() {
+		return numeroNota;
+	}
+	public void setNumeroNota(Integer numeroNota) {
+		this.numeroNota = numeroNota;
+	}
+	public PessoaJuridica getPessoaJuridica() {
+		return pessoaJuridica;
+	}
+	public void setPessoaJuridica(PessoaJuridica pessoaJuridica) {
+		this.pessoaJuridica = pessoaJuridica;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((numeroNota == null) ? 0 : numeroNota.hashCode());
+		result = prime * result + ((pessoaJuridica == null) ? 0 : pessoaJuridica.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompraID other = (CompraID) obj;
+		if (numeroNota == null) {
+			if (other.numeroNota != null)
+				return false;
+		} else if (!numeroNota.equals(other.numeroNota))
+			return false;
+		if (pessoaJuridica == null) {
+			if (other.pessoaJuridica != null)
+				return false;
+		} else if (!pessoaJuridica.equals(other.pessoaJuridica))
+			return false;
+		return true;
+	}
+}
---
+++ modelo_persistencia/src/main/java/com/persistenciaJPA/model/PessoaJuridica.java
@@ -0,0 +1,5 @@
+package com.persistenciaJPA.model;
+
+public class PessoaJuridica {
+
+}

a4f27dc490bdca39c091e2c53edfecd318dcc19c
Your Name <you@example.com>
2018-06-30 10:51:59 -0300

    Classe compra

--- modelo_persistencia/src/main/java/com/persistenciaJPA/model/PessoaJuridica.java
+++ modelo_persistencia/src/main/java/com/persistenciaJPA/model/PessoaJuridica.java
@@ -1,5 +1,42 @@
 package com.persistenciaJPA.model;
 
-public class PessoaJuridica {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+@Entity
+@Table(name = "pessoa_juridica")
+public class PessoaJuridica extends Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "ie", length = 15, nullable = false)
+    private String ie;
+
+    @CNPJ(message = "CNPJ inválido")
+    @Column(name = "cnpj", length = 18, nullable = false)
+    private String cnpj;
+
+    public PessoaJuridica() {
+    }
+
+    public String getIe() {
+        return ie;
+    }
+
+    public void setIe(String ie) {
+        this.ie = ie;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
